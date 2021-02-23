bec268b5be946b91844e1747b926ebd66954b216
camfrogy <smvyawvs@gmail.com>
2021-02-23 16:14:53 +0800

    Added Position and Department class

--- class/Employee.py
+++ class/Employee.py
@@ -5,9 +5,13 @@ class Employee:
 
     def __init__(self):
         self._uid = ''
-        self._name = ''
+        self._full_name = ''
         self._age = 0
         self._address = ''
+        self._email = ''
+        self._employee_image = ''
+        self._contact_number = ''
+
 
     # Setters and Getters for Encapsulation
     @property
@@ -15,43 +19,74 @@ class Employee:
         return self._uid
 
     @uid.setter
-    def uid(self, a):
-        self._uid = a
+    def uid(self, uid):
+        self._uid = uid
 
     @property
-    def name(self):
-        return self._uid
+    def full_name(self):
+        return self._full_name
 
-    @name.setter
-    def name(self, a):
-        self._name = a
+    @full_name.setter
+    def full_name(self, full_name):
+        self._name = full_name
 
     @property
     def age(self):
         return self._age
 
     @age.setter
-    def age(self, a):
+    def age(self, age):
         if age < 0:
             raise ValueError("You're under age.")
-        self._age = a
+        self._age = age
 
     @property
     def address(self):
         return self._address
     
     @address.setter
-    def address(self, a):
-        self._address = a
+    def address(self, address):
+        self._address = address
 
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def contact_number(self):
+        return self._contact_number
+    
+    @contact_number.setter
+    def contact_number(self, number):
+        country_code = "+63"
+        if not len(number) == 11:  # Contact number must in the format of '09XXXXXXXXX'
+            raise ValueError("Contact number should be 11 characters.")
+        number = number[1:]  # Slice the number to remove 0
+        self._contact_number = country_code + number  # Concatenate country code with sliced number
+
+    @property
+    def employee_image(self):
+        return self._employee_image
+
+    @employee_image.setter
+    def employee_image(self, image_path):
+        self._employee_image = image_path
+
+    # Database methods
     # Registering records of employee in database
     def insert_employee_record(self):
         # Create a connection
         con = Connection()
         db = con.connect()
         cursor = db.cursor()
-        query = "INSERT INTO employees(uid, name, age, address) VALUES(%s, %s, %s, %s)"
-        values = (self._uid, self._name, self._age, self._address)
+        query = "INSERT INTO employees(uid, full_name, age, address, email, contact_number, employee_image) \
+            VALUES(%s, %s, %s, %s, %s, %s, %s)"
+        values = (self._uid, self._full_name, self._age, self._address, self._email, self._contact_number\
+            self._employee_image)
         cursor.execute(query, values)
         db.commit()
         db.close()
@@ -74,8 +109,10 @@ class Employee:
         con = Connection()
         db = con.connect()
         cursor = db.cursor()
-        query = "UPDATE employees SET name=%s, age=%s, address=%s WHERE uid=%s"
-        values = (self._name, self._age, self._address, self._uid)
+        query = "UPDATE employees SET full_name=%s, age=%s, address=%s, email=%s, contact_number=%s,\
+            employee_image=%s WHERE uid=%s"
+        values = (self._name, self._age, self._address, self._email, self._contact_number,\
+            self.employee_image, self._uid)
         cursor.execute(query, values)
         db.commit()
         db.close()

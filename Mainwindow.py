bfb4222fb300b310211838b3f1461073889be77d
camfrogy <smvyawvs@gmail.com>
2021-03-02 16:05:12 +0800

    Fixed bugs

--- Mainwindow.py
+++ Mainwindow.py
@@ -1,8 +1,26 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog, messagebox
 from PIL import ImageTk, Image
 from classes.Employee import Employee
-from classes.Connection import Connection
+
+# If database not found, create one then delete the 'con' object
+try:
+    from classes.Connection import Connection
+    con = Connection()
+except:  
+    con.create_database('msc_database')
+finally:
+    del con
+
+# If table 'employees' not found, create one then delete the 'con' object
+try:
+    from classes.Connection import Connection
+    con = Connection()
+    con.create_employee_table('employees')
+except Exception:
+    pass
+finally:
+    del con
 
 
 root=Tk()
@@ -30,14 +48,14 @@ class MainWindow:
         ###Para sa Employee Frame#####       
         def clicked_emp():
             self.frame_emp.lift()
-             #Mga label sa pag insert ng Data
+            #Mga label sa pag insert ng Data
             self.head_label = Label(self.frame_emp, text="Register Employee Information",
             font=("Andalus", 24, 'bold'), fg="white", bg="#339900", borderwidth=10)
             self.head_label.place(x=0, y=20, width=880)
             
-            self.name_label = Label(self.frame_emp, text="First Name", font=("Andalus", 12, 'bold'), 
+            self.fname_label = Label(self.frame_emp, text="First Name", font=("Andalus", 12, 'bold'), 
             fg="black")
-            self.name_label.place(x=20, y=100)
+            self.fname_label.place(x=20, y=100)
             
             self.mname_label = Label(self.frame_emp, text="Middle Name", font=("Andalus", 12, 'bold'), 
             fg="black")
@@ -47,13 +65,13 @@ class MainWindow:
             fg="black")     
             self.lname_label.place(x=20, y=270)
 
-            self.gender_label = Label(self.frame_emp, text="Sex", font=("Andalus", 12, 'bold'),
+            self.gender_label = Label(self.frame_emp, text="Gender", font=("Andalus", 12, 'bold'),
             fg="black")
             self.gender_label.place(x=20, y=350)
 
-            self.bdate_label = Label(self.frame_emp, text="Birth Date", font=("Andalus", 12, 'bold'),
+            self.age_label = Label(self.frame_emp, text="Age", font=("Andalus", 12, 'bold'),
             fg="black")     
-            self.bdate_label.place(x=20, y=430)
+            self.age_label.place(x=20, y=430)
 
             self.address_label = Label(self.frame_emp, text="Address", font=("Andalus", 12, 'bold'),
             fg="black")     
@@ -68,24 +86,28 @@ class MainWindow:
             fg="black")
             self.position_label.place(x=350, y=181)
 
-            self.Salary_label = Label(self.frame_emp, text="Salary Grade", font=("Andalus", 12, 'bold'),
+            self.rfid_label = Label(self.frame_emp, text="RFID UID", font=("Andalus", 12, 'bold'),
             fg="black")
-            self.Salary_label.place(x=350, y=265)
+            self.rfid_label.place(x=350, y=265)
 
             self.email_label = Label(self.frame_emp, text="Email Address", font=("Andalus", 12, 'bold'),
             fg="black")
             self.email_label.place(x=350, y=350)
 
-            self.contact_label = Label(self.frame_emp, text="Contact Details", font=("Andalus", 12, 'bold'),
+            self.contact_label = Label(self.frame_emp, text="Contact Number", font=("Andalus", 12, 'bold'),
             fg="black")
             self.contact_label.place(x=350, y=435)
 
             self.picbox_label = Label(self.frame_emp, text="", bg="darkgreen")
             self.picbox_label.place(x=680, y=125, width=130, height=130)
+
+            self.image_path_label = Label(self.frame_emp, text="Image Path")
+            self.image_path_label.place(x=680, y=260)
+            self.image_path_label.place_forget()
            
             #Mga Entry box para sa pag insert
-            self.name_entry = Entry(self.frame_emp, font=("calibri",14), borderwidth=5)
-            self.name_entry.place(x=20, y=135, width=250)
+            self.fname_entry = Entry(self.frame_emp, font=("calibri",14), borderwidth=5)
+            self.fname_entry.place(x=20, y=135, width=250)
 
             self.mname_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
             self.mname_entry.place(x=20, y=220, width=250)
@@ -93,8 +115,8 @@ class MainWindow:
             self.lname_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
             self.lname_entry.place(x=20, y=305, width=250)
 
-            self.birthdate_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
-            self.birthdate_entry.place(x=20, y=465, width=250)
+            self.age_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
+            self.age_entry.place(x=20, y=465, width=250)
 
             self.address_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
             self.address_entry.place(x=20, y=550, width=250)
@@ -103,8 +125,8 @@ class MainWindow:
             self.email_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
             self.email_entry.place(x=350, y=382, width=250)
 
-            self.email_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
-            self.email_entry.place(x=350, y=463, width=250)
+            self.contact_number_entry = Entry(self.frame_emp, font=("calibri", 14), borderwidth=5)
+            self.contact_number_entry.place(x=350, y=463, width=250)
 
             #combo box for department options
             options_dep = [
@@ -113,7 +135,8 @@ class MainWindow:
                 "SED",
                 "SENG"
             ]
-            self.dep_cbox = ttk.Combobox(self.frame_emp, value=options_dep)
+            self.department_var = StringVar()
+            self.dep_cbox = ttk.Combobox(self.frame_emp, textvariable=self.department_var, value=options_dep)
             self.dep_cbox.current(0)
             self.dep_cbox.place(x=350, y=131, width=250, height=31)
             
@@ -124,26 +147,22 @@ class MainWindow:
                 "College Lecturer",
                 "Instructor I"
             ]
-            self.position_cbox = ttk.Combobox(self.frame_emp, value=options_position)
+            self.position_var = StringVar()
+            self.position_cbox = ttk.Combobox(self.frame_emp, textvariable=self.position_var, value=options_position)
             self.position_cbox.current(0)
             self.position_cbox.place(x=350, y=216, width=250, height=31)
 
-             #combo box for salary Grade
-            options_salaryg = [
-                "","1","2","3","4","5","6","7","8","9",
-                "10","11","12","13","14","15","16","17","18"
-            ]
-            self.salaryg_cbox = ttk.Combobox(self.frame_emp, value=options_salaryg)
-            self.salaryg_cbox.current(0)
-            self.salaryg_cbox.place(x=350, y=302, width=250, height=31)
+            self.rfid_text = Entry(self.frame_emp)
+            self.rfid_text.place(x=350, y=302, width=250, height=31)
 
             #combo box for department options
             options_gender = [
                 "",
-                "MALE",
-                "FEMALE"
+                "Male",
+                "Female"
             ]
-            self.gender_cbox = ttk.Combobox(self.frame_emp, value=options_gender)
+            self.gender_var = StringVar()
+            self.gender_cbox = ttk.Combobox(self.frame_emp, textvariable=self.gender_var, value=options_gender)
             self.gender_cbox.current(0)
             self.gender_cbox.place(x=20, y=385, width=250, height=31)
 
@@ -153,8 +172,15 @@ class MainWindow:
             font=("arial", 15, 'bold'), command=self.register_employee)
             self.btn_register.place(x=20, y=610, width=250, height=50)
 
+            # BROWSE button
+            self.browse_btn = Button(self.frame_emp, text='Browse', command=self.open_image)
+            self.browse_btn.place(x=680, y=302)
+
+        # Department Tab
         def clicked_dep():
             self.frame_dep.lift()
+        
+        # Position Tab
         def clicked_pos():
             self.frame_pos.lift()
            
@@ -224,8 +250,57 @@ class MainWindow:
 
     # REGISTERING Employee
     def register_employee(self):
+        self.check_if_empty()
+
         employee = Employee()
+        employee.uid = self.rfid_text.get()
+        employee.first_name = self.fname_entry.get()
+        employee.middle_name = self.mname_entry.get()
+        employee.last_name = self.lname_entry.get()
+        employee.age = self.age_entry.get()
+        employee.gender = self.gender_var.get()
+        employee.address = self.address_entry.get()
+        employee.email = self.email_entry.get()
+        employee.employee_image = self.image_path_label.cget('text')
+        employee.contact_number = self.contact_number_entry.get()
+        employee.insert_employee_record()
         
+        self.clear_text()
+
+    # File Dialog for browsing image
+    def open_image(self):
+        self.file_name = filedialog.askopenfilename(initialdir = "/", title="Select a Photo",
+        filetype=(("jpeg","*.jpg"),("png","*.png")))
+        self.image_path_label.configure(text = self.file_name)
+        self.image = ImageTk.PhotoImage(Image.open(self.file_name))
+        self.picbox_label = ttk.Label(self.frame_emp, image=self.image)
+        self.picbox_label.place(x=680, y=125, width=130, height=130)
+        # Debugging purpose
+        # print(self.image_path_label.cget('text'))
+
+    def check_if_empty(self):
+        if self.fname_entry.get() == '' or self.mname_entry.get() == '' or self.lname_entry.get() == '' \
+            or self.age_entry.get() == '' or self.email_entry.get() == '' or self.address_entry.get() == '' \
+            or self.contact_number_entry.get() == '' or self.gender_var.get() == '' \
+            or self.position_var.get() == '' or self.department_var.get() == '' \
+            or self.image_path_label.cget('text') == '' or self.rfid_text.get() == '':
+            messagebox.showwarning("Empty field(s)", "Please fill out all the form.")
+
+    def clear_text(self):
+        self.fname_entry.delete(0, 'end')
+        self.mname_entry.delete(0, 'end')
+        self.lname_entry.delete(0, 'end')
+        self.age_entry.delete(0, 'end')
+        self.address_entry.delete(0, 'end')
+        self.email_entry.delete(0, 'end')
+        self.contact_number_entry.delete(0, 'end')
+        self.image_path_label.configure(text="")
+        self.gender_cbox.current(0)
+        self.dep_cbox.current(0)
+        self.position_cbox.current(0)
+        self.rfid_text.delete(0, 'end')
+
+
 
 main=MainWindow(root)
 root.mainloop()
--- classes/Connection.py
+++ classes/Connection.py
@@ -6,13 +6,7 @@ class Connection:
     HOST = 'localhost'
     USER = 'root'
     PASS = ''
-    DATABASE = 'tkinter'
-
-    # def __init__(self):
-    #     HOST = "localhost"
-    #     USER = "root"
-    #     PASS = ""
-    #     DATABASE = "msc_database"
+    DATABASE = 'msc_database'
 
     def connect(self):
         con = mysql.connector.connect(
@@ -23,9 +17,45 @@ class Connection:
             )
         return con
 
-    def create_database(self, database):
+    def create_database(self, database_name):
         db = mysql.connector.connect(host=self.HOST, user=self.USER, password=self.PASS)
         cursor = db.cursor()
-        query = f"CREATE DATABASE {database}"
+        query = f"CREATE DATABASE {database_name};"
+        cursor.execute(query)
+        db.close()
+
+    def if_employee_table_not_exist(self):
+        table_name = 'employee'
+
+        db = self.connect()
+        cursor = db.cursor()
+        query = "SELECT * FROM information_schema.tables WHERE table_name=%s;"
+        value = (table_name,)
+        cursor.execute(query, value)
+        result = cursor.fetchall()
+        db.close()
+            # raise Exception("Table does not exist.")
+            # print("Table does not exist.")
+        print(result)
+        return len(result) == 0
+
+    def create_employee_table(self, table_name):
+        db = self.connect()
+        cursor = db.cursor()
+        query = f"""CREATE TABLE {table_name}(
+            employee_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            department_id INT(11) NULL,
+            position_id INT(11) NULL,
+            rfid_uid VARCHAR(10) NOT NULL,
+            first_name VARCHAR(50) NOT NULL,
+            middle_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            age INT(2) NOT NULL,
+            gender VARCHAR(10) NOT NULL,
+            email VARCHAR(20) NOT NULL,
+            address VARCHAR(5) NOT NULL,
+            contact_number VARCHAR(10) NOT NULL,
+            employee_image VARCHAR(20) NOT NULL
+        )"""
         cursor.execute(query)
         db.close()
